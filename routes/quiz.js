4f9afa2743a5095edda19ca5dc3a8448e9aff590
Katarzyna Lubecka <katarzyna.lubecka93@gmail.com>
2024-07-26 16:43:42 +0200

    Quiz form

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz');
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('quiz', { title: 'Quiz' });
+router.get('/', async (req, res) => {
+  const show = !req.session.vote;
+  try {
+    let sum = 0;
+    const data = await Quiz.find({});
+    data.forEach((item) => {
+      sum += item.vote;
+    });
+
+    res.render('quiz', { title: 'Quiz', data, show, sum });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  const id = req.body.quiz;
+
+  try {
+    const data = await Quiz.findOne({ _id: id });
+    data.vote = data.vote + 1;
+    try {
+      data.save();
+      req.session.vote = 1;
+      res.redirect('/quiz');
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
