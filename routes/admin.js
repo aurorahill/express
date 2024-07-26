3038030cca8cb20a029c045933248497181e49ec
Katarzyna Lubecka <katarzyna.lubecka93@gmail.com>
2024-07-26 13:54:29 +0200

    Admin news

--- routes/admin.js
+++ routes/admin.js
@@ -12,29 +12,44 @@ router.all('*', (req, res, next) => {
 });
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  // const newsData = new News({
-  //   title: 'Tytul testowy',
-  //   description: 'Opis...',
-  // });
+router.get('/', async (req, res) => {
+  const data = await News.find({});
+  // console.log(data);
 
-  // async function main() {
-  //   try {
-  //     await newsData.save();
-  //   } catch (err) {
-  //     console.log(err);
-  //   } finally {
-  //     // await mongoose.disconnect();
-  //   }
-  // }
-
-  // main();
-
-  res.render('admin/index', { title: 'Admin' });
+  res.render('admin/index', { title: 'Admin', data });
 });
 
 router.get('/news/add', (req, res) => {
-  res.render('admin/news-form', { title: 'Dodaj news' });
+  res.render('admin/news-form', { title: 'Dodaj news', body: {}, errors: {} });
+});
+
+router.post('/news/add', async (req, res) => {
+  const body = req.body;
+
+  const newsData = new News(body);
+  const errors = newsData.validateSync();
+
+  try {
+    await newsData.save();
+    res.redirect('/admin');
+  } catch (err) {
+    console.log(err);
+    if (err) {
+      res.render('admin/news-form', { title: 'Dodaj news', errors, body });
+      return;
+    }
+  } finally {
+    // await mongoose.disconnect();
+  }
+});
+
+router.get('/news/delete/:id', async (req, res) => {
+  try {
+    await News.findByIdAndDelete(req.params.id);
+  } catch (err) {
+    console.log(err);
+  }
+  res.redirect('/admin');
 });
 
 module.exports = router;
