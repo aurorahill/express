33be553a8752de8fa17199eadc0cfe045f29efa1
Katarzyna Lubecka <katarzyna.lubecka93@gmail.com>
2024-07-26 15:50:42 +0200

    News filter front

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news');
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('news', { title: 'News' });
+router.get('/', async (req, res) => {
+  const search = req.query.search || '';
+  let data;
+  try {
+    data = await News.find({ title: new RegExp(search.trim(), 'i') })
+      .sort({ created: -1 })
+      .exec();
+
+    // data.exec();
+  } catch (err) {
+    console.log(err);
+  }
+  res.render('news', { title: 'News', data, search });
 });
 
 module.exports = router;
